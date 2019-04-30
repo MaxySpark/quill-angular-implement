b608ee39345daa8cdd1928aa24d2999fe0200f3e
MaxySpark <me@maxyspark.me>
2019-04-30 18:44:28 +0530

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { EditorModule } from './editor/editor.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/editor/editor/editor.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css']
+})
+export class EditorComponent implements OnInit {
+
+  editorForm: FormGroup;
+
+  editorStyle = {
+    height : '400px'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+    this.editorForm = new FormGroup({
+      editor: new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.editorForm.get('editor').value);
+  }
+
+}
